0c3d35ef0da91435f746dd8f07a598234e75771e
Duyen <duyenctph39530@fpt.edu.vn>
2023-12-11 03:19:25 +0700

    thống kê

--- src/Controllers/Admin/DashboardController.php
+++ src/Controllers/Admin/DashboardController.php
@@ -3,16 +3,21 @@
 namespace Ductong\BaseMvc\Controllers\Admin;
 
 use Ductong\BaseMvc\Controller;
-
+use Ductong\BaseMvc\Models\Dashboard;
 
 class DashboardController extends Controller {
-
     
     public function index() {
-       
+        $dashboard = (new Dashboard())->getProductCountByCategory();
+        $totalQuantity = (new Dashboard())->getTotalQuantity();
+        $totalUser =(new Dashboard())->getTotalUser();
+        $totalBill =(new Dashboard())->getTotalBill();
 
-        $this->renderAdmin("dashboard");
+        $this->renderAdmin('dashboard', [
+            'dashboard' => $dashboard,
+            'totalQuantity' => $totalQuantity,
+            'totalUser' => $totalUser,
+            'totalBill' => $totalBill,
+        ]);
     }
-
-
 }
